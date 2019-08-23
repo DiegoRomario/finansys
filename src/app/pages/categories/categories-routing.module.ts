9630850373d4d043f649ea0f73dc306217b0c624
Diego Romario <diego.romario@outlook.com>
2019-08-22 21:34:45 -0300

    including category-list component

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,7 +1,8 @@
-import { CategoryFormComponent } from './category-form/category-form.component';
-import { CategoryListComponent } from './category-list/category-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 const routes: Routes = [
   { path: '', component: CategoryListComponent },
