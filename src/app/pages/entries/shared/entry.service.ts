ac529298ef7d81729493aecf9a9ca9955fac7131
Diego Romario <diego.romario@outlook.com>
2019-09-01 17:09:41 -0300

    finishing the second module

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,9 +1,10 @@
+import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Entry } from './entry.model';
 
-import {map, catchError, flatMap} from 'rxjs/operators';
+import { map, catchError, flatMap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,7 +14,7 @@ export class EntryService {
 
   private apiPath: string = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -24,25 +25,40 @@ export class EntryService {
 
   getById(id: number): Observable<Entry> {
     const url = this.apiPath + '/' + id;
-    return this.http.get(url).pipe (
+    return this.http.get(url).pipe(
       catchError(this.handleError),
       map(this.jsonDataToEntry)
     )
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+
+      })
     );
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = this.apiPath + '/' + entry.id;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
-    );
+
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+
+      }));
   }
 
   delete(id: number): Observable<any> {
