e9db655e77cd4d39f436541f1469e7d080d17388
Diego Romario <diego.romario@outlook.com>
2019-09-09 22:28:53 -0300

    improving components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 @NgModule({
   declarations: [
     BreadCrumbComponent,
-    PageHeaderComponent],
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule, RouterModule
@@ -18,7 +22,9 @@ import { PageHeaderComponent } from './components/page-header/page-header.compon
     ReactiveFormsModule,
     BreadCrumbComponent,
     RouterModule,
-    PageHeaderComponent
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ]
 })
 export class SharedModule { }
