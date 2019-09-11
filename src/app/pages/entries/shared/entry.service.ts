2e97a1dbfdeb736f16b6d7f0943f3322714e6b4d
Diego Romario <diego.romario@outlook.com>
2019-09-10 21:37:43 -0300

    adding reports component

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
-import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable, Injector } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+
+import { Observable } from 'rxjs';
+import { flatMap, catchError, map } from 'rxjs/operators';
+
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
+import { CategoryService } from '../../categories/shared/category.service';
 import { Entry } from './entry.model';
 
-import { map, catchError, flatMap } from 'rxjs/operators';
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import * as moment from 'moment'
 
 
 @Injectable({
@@ -13,8 +15,7 @@ import { BaseResourceService } from 'src/app/shared/services/base-resource.servi
 })
 export class EntryService extends BaseResourceService<Entry> {
 
-
-  constructor(protected injector: Injector, private categoryService: CategoryService) {
+  constructor(protected injector: Injector, private categoryService: CategoryService) { 
     super('api/entries', injector, Entry.fromJson);
   }
 
@@ -24,18 +25,34 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   update(entry: Entry): Observable<Entry> {
-    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
   }
 
 
-  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-        return sendFn(entry);
-      }), catchError(this.handleError));
+        return sendFn(entry)
+      }),
+      catchError(this.handleError)
+    );
   }
 
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
 
+      if(monthMatches && yearMatches) return entry;
+    })
+  }
 
 }
